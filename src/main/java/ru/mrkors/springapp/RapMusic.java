d99059a7ab675a21e1448f082564321901be5df9
Duma <mrkorsar@list.ru>
2019-08-09 00:27:46 +0300

    Init, destroy and factory methods.

--- src/main/java/ru/mrkors/springapp/RapMusic.java
+++ src/main/java/ru/mrkors/springapp/RapMusic.java
@@ -6,6 +6,21 @@ package ru.mrkors.springapp;
  */
 public class RapMusic implements Music{
 
+    private RapMusic() {
+    }
+    
+    public static RapMusic setRapMusic(){
+        return new RapMusic();
+    }
+    
+    public void InitMethod(){
+        System.out.println("Initialization...");
+    }
+    public void DestroyMethod(){
+        System.out.println("Destruction.");
+    }
+    
+
     @Override
     public String getSong() {
         return "Pizza";
--- src/main/java/ru/mrkors/springapp/TestSpring.java
+++ src/main/java/ru/mrkors/springapp/TestSpring.java
@@ -7,21 +7,8 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         
-//        Music music = context.getBean("musicBean", Music.class);
-//        MusicPlayer musicplayer = new MusicPlayer(music);
-        MusicPlayer firstmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        
-        boolean comparison = firstmusicPlayer == secondmusicPlayer;
-        System.err.println(comparison);
-        
-        firstmusicPlayer.setVolume(10);
-        
-        System.out.println(firstmusicPlayer.getVolume());
-        System.out.println(secondmusicPlayer.getVolume());
-//        musicPlayer.playSong();
-//        System.out.println(musicPlayer.getName());
-//        System.out.println(musicPlayer.getVolume());
+        RapMusic rapMusic = context.getBean("musicBean", RapMusic.class);
+        System.out.println(rapMusic.getSong());
         
         context.close();
     }
